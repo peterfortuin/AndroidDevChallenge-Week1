6f248568a8e474d05a3ad726c826b519adfa0b4c
Peter Fortuin <peter.fortuin@gmail.com>
2021-02-26 11:38:06 +0100

    Puppy loading animation fixes

--- app/src/main/java/com/example/androiddevchallenge/ui/PuppyAdoptionListScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/PuppyAdoptionListScreen.kt
@@ -15,17 +15,13 @@
  */
 package com.example.androiddevchallenge.ui
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredHeight
-import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.BottomAppBar
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
@@ -33,10 +29,8 @@ import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.airbnb.lottie.compose.LottieAnimation
@@ -58,9 +52,6 @@ fun PuppyAdoptionScreen() {
                     Text(text = "Adopt a pup!")
                 }
             )
-        },
-        bottomBar = {
-            BottomAppBar {}
         }
     ) {
         PuppyList()
@@ -100,32 +91,38 @@ fun PuppyItem(puppy: Puppy) {
 
             val animationSpec = remember { LottieAnimationSpec.RawRes(R.raw.french_bulldog) }
 
-            GlideImage(
-                data = puppy.picture,
-            ) { imageState ->
-                when (imageState) {
-                    is ImageLoadState.Success -> {
-                        MaterialLoadingImage(
-                            result = imageState,
-                            puppy.name,
-                            fadeInEnabled = true,
-                            fadeInDurationMs = 600,
-                            modifier = Modifier.width(150.dp)
-                        )
-                    }
-                    is ImageLoadState.Error -> {
-                        LottieAnimation(
-                            animationSpec,
-                            modifier = Modifier.requiredWidth(80.dp).requiredHeight(150.dp)
-                        )
-                    }
-                    is ImageLoadState.Loading -> {
-                        LottieAnimation(
-                            animationSpec,
-                            modifier = Modifier.requiredWidth(80.dp).requiredHeight(150.dp)
-                        )
-                    }
-                    is ImageLoadState.Empty -> {
+            Row(
+                modifier = Modifier
+                    .width(150.dp)
+                    .height(150.dp)
+            ) {
+                GlideImage(
+                    data = puppy.picture,
+                    modifier = Modifier.fillMaxWidth()
+                ) { imageState ->
+                    when (imageState) {
+                        is ImageLoadState.Success -> {
+                            MaterialLoadingImage(
+                                result = imageState,
+                                puppy.name,
+                                fadeInEnabled = true,
+                                fadeInDurationMs = 600,
+                            )
+                        }
+                        is ImageLoadState.Error -> {
+                            LottieAnimation(
+                                animationSpec,
+                                modifier = Modifier.align(Alignment.Center)
+                            )
+                        }
+                        is ImageLoadState.Loading -> {
+                            LottieAnimation(
+                                animationSpec,
+                                modifier = Modifier.align(Alignment.Center)
+                            )
+                        }
+                        is ImageLoadState.Empty -> {
+                        }
                     }
                 }
             }
